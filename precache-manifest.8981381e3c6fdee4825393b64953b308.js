0b78ae39fba0de43bb0508db5c89ac7b7b27dadd
SebastienBosca <sbosca@wanadoo.fr>
2022-06-28 10:54:56 +0200

    Updates

---
+++ precache-manifest.8981381e3c6fdee4825393b64953b308.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccab230d6e5d48cc0e445bd348d69b2e",
+    "url": "/NFT/index.html"
+  },
+  {
+    "revision": "dda0e84d5d3e1fd8d1c1",
+    "url": "/NFT/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "85b67661e18fa91c5b6f",
+    "url": "/NFT/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "dda0e84d5d3e1fd8d1c1",
+    "url": "/NFT/static/js/2.dd6421bb.chunk.js"
+  },
+  {
+    "revision": "85b67661e18fa91c5b6f",
+    "url": "/NFT/static/js/main.082921f7.chunk.js"
+  },
+  {
+    "revision": "571df9e8e318ab95a4d5",
+    "url": "/NFT/static/js/runtime-main.0a9070f0.js"
+  }
+]);
